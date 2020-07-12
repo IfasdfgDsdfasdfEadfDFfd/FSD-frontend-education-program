b09aa511ad0e4dc0af616f5516ec7f394f4a7399
Unknown user <gugfug1@gmail.com>
2020-07-12 06:49:49 +0000

    rewrite all script

--- src/components/range-slider/range-slider.ts
+++ src/components/range-slider/range-slider.ts
@@ -1,48 +1,86 @@
-const leftInput = document.querySelector('.range-slider__element.range-slider__element-left') as HTMLInputElement;
-const rightInput = document.querySelector('.range-slider__element.range-slider__element-right') as HTMLInputElement;
-
-const sliderRange = document.querySelector('.slider > .slider__range') as HTMLElement;
-const sliderTrack = document.querySelector('.slider > .slider__track') as HTMLElement;
-const leftSliderThumb = document.querySelector('.slider > .slider__thumb_left') as HTMLElement;
-const rightSliderThumb = document.querySelector('.slider > .slider__thumb_right') as HTMLElement;
+import { createStore, saveToLocalStoragePlugin, saveToLocalStorageMiddlewareFabric, Action } from '../../globals/store';
 
 
-const setLeftInputValue = (event: any) => {
-  const value = Math.min(parseInt(event.target.value), parseInt(rightInput.value));
-  event.target.value = String(value);
+const sliders = Array(...document.getElementsByClassName('range-slider') as unknown as Array<HTMLElement>);
+const sliderStorageName = 'range-slider-data';
+const defaultValue = {left: 0, right: 0, min: 0, max: 100};
 
-  const { min, max } = event.target;
-
-  const percent = ((value - min) / (max - min)) * 100;
-
-  const offset = Math.round((
-    sliderTrack.clientWidth * percent / 100
-  ) - (
-    leftSliderThumb.clientWidth * percent / 100
-  ));
-
-  leftSliderThumb.style.left = `${offset}px`;
-  sliderRange.style.left = `${percent}%`;
+enum actions {
+  CHANGE_LEFT_THUMB,
+  CHANGE_RIGHT_THUMB,
+  SET_MIN_VALUE,
+  SET_MAX_VALUE,
 };
 
-const setRightInputValue = (event: any) => {
-  const value = Math.max(parseInt(event.target.value), parseInt(leftInput.value));
-  event.target.value = String(value);
-
-  const { min, max } = event.target;
-
-  const percent = ((value - min) / (max - min)) * 100;
-
-  const offset = Math.round((
-    sliderTrack.clientWidth * percent / 100
-  ) - (
-    rightSliderThumb.clientWidth * percent / 100
-  ));
-
-  rightSliderThumb.style.left = `${offset}px`;
-  sliderRange.style.right = `${100 - percent}%`;
+const reducer = (action: Action, state: any) => {
+  switch (action.name) {
+    case actions.CHANGE_LEFT_THUMB:
+      return {...state, [action.value.id]: {...state[action.value.id], left: action.value.cost}};
+    case actions.CHANGE_RIGHT_THUMB:
+      return {...state, [action.value.id]: {...state[action.value.id], right: action.value.cost}};
+    case actions.SET_MIN_VALUE:
+      return {...state, [action.value.id]: {...state[action.value.id], min: action.value.min}}
+    case actions.SET_MAX_VALUE:
+      return {...state, [action.value.id]: {...state[action.value.id], max: action.value.max}}
+    default:
+      return state;
+  }
 };
 
 
-leftInput?.addEventListener('input', setLeftInputValue);
-rightInput?.addEventListener('input', setRightInputValue);
+const store = createStore({},
+  reducer,
+  [saveToLocalStoragePlugin(sliderStorageName, sliders, defaultValue)],
+  {post: [saveToLocalStorageMiddlewareFabric(sliderStorageName)]}
+);
+
+sliders.forEach(slider => {
+  store.subscribe(state => {
+    const {min, max, left, right} = state[slider.id];
+    const leftThumb = slider.querySelector('.slider__thumb_left') as HTMLElement;
+    const rightThumb = slider.querySelector('.slider__thumb_right') as HTMLElement;
+    const range = slider.querySelector('.slider__range') as HTMLElement;
+
+    const leftPercent = (left-min) / (max-min) * 100;
+    const rightPercent = (right-min) / (max-min) * 100;
+
+    leftThumb.style.left = `${leftPercent - (0.05 * leftPercent) - 1}%`;
+    rightThumb.style.left = `${rightPercent - (0.05 * rightPercent)}%`;
+    range.style.left = `${leftPercent}%`;
+    range.style.right = `${100 - rightPercent}%`;
+  });
+
+  const inputElement = slider.querySelector('.range-slider__element') as HTMLInputElement;
+
+  store.dispatch({name: 'COLD_START'});
+  store.dispatch({name: actions.SET_MIN_VALUE, value: {id: slider.id, min: parseInt(inputElement.min)}});
+  store.dispatch({name: actions.SET_MAX_VALUE, value: {id: slider.id, max: parseInt(inputElement.max)}});
+
+  const {left, right} = store.getState()[slider.id];
+
+  const leftThumbEl = slider.querySelector('.range-slider__element-left') as HTMLInputElement;
+  leftThumbEl.value = left;
+  leftThumbEl.addEventListener('input', (event: any) => {
+    const value = Math.min(parseInt(event.target.value), store.getState()[slider.id].right);
+
+    event.target.value = String(value);
+
+    store.dispatch({name: actions.CHANGE_LEFT_THUMB, value: {
+      id: event.target.parentElement.id,
+      cost: value,
+    }});
+  });
+
+  const rightThumbEl = slider.querySelector('.range-slider__element-right') as HTMLInputElement;
+  rightThumbEl.value = right;
+  rightThumbEl.addEventListener('input', (event: any) => {
+    const value = Math.max(parseInt(event.target.value), store.getState()[slider.id].left);
+
+    event.target.value = String(value);
+
+    store.dispatch({name: actions.CHANGE_RIGHT_THUMB, value: {
+      id: event.target.parentElement.id,
+      cost: value,
+    }});
+  });
+});
