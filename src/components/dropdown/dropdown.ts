cd4990c2c9c0ebe04047c472925f6ed9bf335547
gugfug <gugfug1@gmail.com>
2020-10-11 01:27:36 +0000

    it's about dropdown #2

--- src/components/dropdown/dropdown.ts
+++ src/components/dropdown/dropdown.ts
@@ -1,7 +1,11 @@
 const dropdowns = Array(...document.getElementsByClassName('dropdown-container') as unknown as Array<HTMLElement>);
 
 dropdowns.forEach(dropdown => {
-  dropdown.addEventListener('click', () => {
-    dropdown.classList.toggle('dropdown-container--opened');
+  dropdown.addEventListener('click', event => {
+    const target = event.target as HTMLElement;
+
+    if (target.nodeName === 'BUTTON' || target.parentElement?.nodeName === 'BUTTON') {
+      dropdown.classList.toggle('dropdown-container--opened');
+    }
   });
 })
