cc3a27a1078b5d1a7fb5ee3d15779a124a882d16
gugfug <gugfug1@gmail.com>
2020-11-23 08:35:58 +0000

    add ASSET_PATH env variable

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           outputPath: 'static',
-          publicPath: 'static',
+          publicPath: process.env.ASSET_PAHT || '/',
           name: '[name].[ext]',
         },
       },
