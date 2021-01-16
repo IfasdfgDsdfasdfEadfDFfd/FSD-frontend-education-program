4a633d097c74701d8fce1f13afa3a966b77104dc
gugfug <gugfug1@gmail.com>
2021-01-16 21:41:00 +1000

    fix header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -6,14 +6,6 @@ const attachingClass = 'header_attached';
 let lastScrollYPosition = window.scrollY;
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY !== 0) {
-    if (!header?.classList.contains(attachingClass)) {
-      header?.classList.add(attachingClass);
-    }
-  } else {
-    header?.classList.remove(attachingClass);
-  }
-
   if (lastScrollYPosition < window.scrollY) {
     if (!header?.classList.contains(hidingClass)) {
       header?.classList.add(hidingClass);
